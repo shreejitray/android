5cbcabced6ab42da75ae5be74da14a6521715e19
shreejitray <shreejit.1@gmail.com>
2019-04-22 23:09:24 +0530

    first commit:

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE="com.example.myfristApp.Message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.editText2);
+        String messages = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,messages);
+        startActivity(intent);
+    }
+}
